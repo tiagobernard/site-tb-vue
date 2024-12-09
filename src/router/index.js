0b2d155c29f2f8c10d5b349fcd54e2067de42f20
tiagobernard <tiago@tiagobernardes.com.br>
2024-12-09 18:38:22 -0300

    Modificação do scroll

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,30 @@ const routes = [
 ]
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return new Promise((resolve) => {
+        // Aguarda a navegação para a nova rota ser concluída
+        setTimeout(() => {
+          const element = document.querySelector(to.hash);
+          if (element) {
+            const headerHeight = 108; // Altura do cabeçalho fixo
+            const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+            const offsetPosition = elementPosition - headerHeight;
+  
+            window.scrollTo({
+              top: offsetPosition,
+              behavior: "smooth",
+            });
+          }
+          resolve(); // Finaliza a Promise
+        }, 750); // Reduza ou ajuste conforme necessário
+      });
+    }
+  
+    return savedPosition || { top: 0 }; // Caso padrão: rola para o topo ou posição salva
+  }
+});
 
 export default router
