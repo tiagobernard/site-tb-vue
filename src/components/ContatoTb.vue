6d685a983b63e748aeca8eaf7670d8266e20c820
tiagobernard <tiago@tiagobernardes.com.br>
2024-12-14 12:02:21 -0300

    Adicionando animação nos links do componente  contato

--- src/components/ContatoTb.vue
+++ src/components/ContatoTb.vue
@@ -30,7 +30,8 @@ export default {
 .contato {background:#e8e8e8;padding:35px 0;}
 .contato h2,.contato h3 {font-family:'Montserrat';font-weight:400}
 .contato h3 {margin-bottom:35px;}
-.contato a {color:#3f729b;text-decoration:none;font-weight:600}
+.contato a {color:#3f729b;text-decoration:none;font-weight:600;display: inline-block;}
+.contato a:hover {transition: 1s;transform: scale(0.9)}
 input, textarea {font-family:"Space Mono", Helvetica, Arial, Verdana, sans-serif;transition:.3s;outline:none}
 input:focus, textarea:focus {padding:10px;}
 textarea {height:100px;}
